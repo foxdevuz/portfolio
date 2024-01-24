417c96536e170cad5baede1c117b3aab5e37bd61
abdullajonov.uz <abdullajonovboburjon05@gmail.com>
2024-01-24 21:05:01 +0500

    [upd][structure] Link in main

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -17,8 +17,6 @@
     <!-- Include counterUp script before main.js -->
     <script src="{{ asset('assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
 
-    <!-- Include main.js -->
-    <script src="{{ asset('assets/js/main.js') }}"></script>
 
     <!-- Vendor CSS Files -->
     <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
@@ -62,6 +60,8 @@
 
 {{ $slot  }}
 
+<!-- Include main.js -->
+<script src="{{ asset('assets/js/main.js') }}"></script>
 <script src="{{ asset('assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
 <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ asset('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
